a5470125b467cc5ac1e83d14d6661364ab3586a3
Sarah Drew <sarahdrew93@gmail.com>
2019-04-11 07:59:19 -0500

    added security and formatted for deployment

--- server.js
+++ server.js
@@ -1,56 +1,67 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 //bring in movies
-const movies = require('./movies-data-small.json')
+const movies = require("./movies-data-small.json");
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(helmet())
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
+app.use(cors());
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
   //from env doc
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
   }
   //next to have it move on
-  next()
-})
+  next();
+});
 
-app.get('/movie', function handleGetMovie(req, res) {
+app.get("/movie", function handleGetMovie(req, res) {
   let response = movies;
 
   if (req.query.genre) {
     //allow to have lowercase searches
     response = response.filter(movie =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    )
+    );
   }
 
   if (req.query.country) {
     response = response.filter(movie =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-    )
+    );
   }
 
   if (req.query.avg_vote) {
     //greater than or equal to supplied number
-    response = response.filter(movie =>
-      Number(movie.avg_vote) >= Number(req.query.avg_vote)
-    )
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
   }
 
-  res.json(response)
-})
+  res.json(response);
+});
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 //crate a port
-const PORT = 9000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is now listening at http://localhost:${PORT}`)
-})
+  console.log(`Server is now listening at http://localhost:${PORT}`);
+});
